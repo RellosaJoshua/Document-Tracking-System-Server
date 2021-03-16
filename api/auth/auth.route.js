79ca854f40c88b3576ff705c0bf7c0655769fefc
joyul <bg201802211@wmsu.edu.ph>
2021-03-16 19:57:07 +0800

    token verification

--- api/auth/auth.route.js
+++ api/auth/auth.route.js
@@ -9,6 +9,6 @@ const router = express.Router();
 
 router.post('/login', login);
 router.post('/isauth', isAuth);
-router.get('/authuser', [verifyAuthUser], getAuthUser);
+router.get('/authuser', [verifyToken, verifyAuthUser], getAuthUser);
 
 module.exports = router;
